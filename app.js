25f01f326648ba5f25c61dba9d3b7a7d43006ef6
ErykF07 <frackowiak.eryk@gmail.com>
2023-11-25 19:11:04 +0100

    finished version

---
+++ app.js
@@ -0,0 +1,27 @@
+const ratingComponent = document.querySelector(".box__rating");
+const submittedComponent = document.querySelector(".box__submitted");
+const submitBtn = document.querySelector(".rating__submit");
+const ratingButtons = document.querySelectorAll(".rating__number");
+const score = document.querySelector(".submitted__score");
+let rating;
+
+function updateRating(selectedButton) {
+  ratingButtons.forEach((button) => {
+    button.classList.remove("selected"); // removing .selected from each button
+  });
+
+  rating = selectedButton.textContent;
+  selectedButton.classList.add("selected"); // adding .selected to the selected button
+  score.textContent = rating; // updating .rating__submit button
+}
+
+function updateComponent() {
+  ratingComponent.classList.add("hide");
+  submittedComponent.classList.add("show");
+}
+
+submitBtn.addEventListener("click", updateComponent);
+
+ratingButtons.forEach((btn) => {
+  btn.addEventListener("click", () => updateRating(btn));
+});
